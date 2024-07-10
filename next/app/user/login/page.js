e89241fa17a91574e5dff2a0ee55c369622ac4e9
beginerKosukeT <kosuke44.tamaguy@gmail.com>
2024-07-11 03:01:30 +0900

    微修正

--- next/app/user/login/page.js
+++ next/app/user/login/page.js
@@ -8,7 +8,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/user/register`, {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/user/login`, {
                 method: "POST",
                 headers: {
                     "Accept": "application/json",
