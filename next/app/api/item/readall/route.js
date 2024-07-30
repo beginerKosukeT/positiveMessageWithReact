def5e4daa63ec19ad292abb51e6bf3f778b90915
beginerKosukeT <kosuke44.tamaguy@gmail.com>
2024-07-30 09:23:49 +0900

    微修正

--- next/app/api/item/readall/route.js
+++ next/app/api/item/readall/route.js
@@ -5,7 +5,7 @@ import { ItemModel } from "../../../utils/schemaModels"
 export async function GET() {
     try {
         await connectDB()
-        const allItems = await ItemModel.find().sort({ createdAt: 'desc'})
+        const allItems = await ItemModel.find().limit(8).sort({ createdAt: 'desc'})
         return NextResponse.json({ message: "アイテム読み取り成功（オール）", allItems: allItems })
     } catch (err) {
         return NextResponse.json({ message: "アイテム読み取り失敗（オール）" })
