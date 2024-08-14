b29cac9b98342de2e5309f732d7a3206e98df9e3
beginerKosukeT <kosuke44.tamaguy@gmail.com>
2024-08-14 16:13:19 +0900

    微修正

--- next/app/item/search/page.js
+++ next/app/item/search/page.js
@@ -7,6 +7,7 @@ import Image from "next/image"
 const SearchItems = () => {
     const [keyword, setKeyword] = useState("")
     const [foundItems, setFoundItems] = useState([])
+    const [searched, setSearched] = useState(false)
 
     const getFoundItems = async (e) => {
         e.preventDefault()
@@ -25,31 +26,59 @@ const SearchItems = () => {
         const jsonData = await response.json()
         const foundItems = jsonData.foundItems
         setFoundItems(foundItems)
+        setSearched(true)
     }
 
-    return (
-        <div>
-            <h1 className="page-title">検索</h1>
-            <form onSubmit={getFoundItems}>
-                <input value={keyword} onChange={(e) => setKeyword(e.target.value)} type="text" name="keyword" placeholder="キーワード" required />
-                <button className={foundItems.length === 0 ? "" : "margin-bottom"}>検索</button>
-            </form>
-            <div className="grid-container-in">
-                {foundItems.map(item =>
-                    <Link href={`/item/readsingle/${item._id}`} key={item._id} >
-                        <div className="display-flex">
-                            <Image src={item.image} width={750} height={500} alt="item-image" priority />
-                            <div>
-                                <h2>{item.title}</h2>
-                                <h3>{item.author}</h3>
-                                <p>{item.message.substring(0, 10)}...</p>
-                            </div>
-                        </div>
-                    </Link>
-                )}
+    if (searched) {
+        if (foundItems.length === 0) {
+            return (
+                <div>
+                    <h1 className="page-title">検索</h1>
+                    <form onSubmit={getFoundItems}>
+                        <input value={keyword} onChange={(e) => setKeyword(e.target.value)} type="text" name="keyword" placeholder="キーワード" required />
+                        <button>検索</button>
+                    </form>
+                    <div className="basic-font margin-top">
+                        該当する投稿が見つかりませんでした。
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <h1 className="page-title">検索</h1>
+                    <form onSubmit={getFoundItems}>
+                        <input value={keyword} onChange={(e) => setKeyword(e.target.value)} type="text" name="keyword" placeholder="キーワード" required />
+                        <button className="margin-bottom">検索</button>
+                    </form>
+                    <div className="grid-container-in">
+                        {foundItems.map(item =>
+                            <Link href={`/item/readsingle/${item._id}`} key={item._id} >
+                                <div className="display-flex">
+                                    <Image src={item.image} width={750} height={500} alt="item-image" priority />
+                                    <div>
+                                        <h2>{item.title}</h2>
+                                        <h3>{item.author}</h3>
+                                        <p>{item.message.substring(0, 10)}...</p>
+                                    </div>
+                                </div>
+                            </Link>
+                        )}
+                    </div>
+                </div>
+            )
+        }
+    } else {
+        return (
+            <div>
+                <h1 className="page-title">検索</h1>
+                <form onSubmit={getFoundItems}>
+                    <input value={keyword} onChange={(e) => setKeyword(e.target.value)} type="text" name="keyword" placeholder="キーワード" required />
+                    <button>検索</button>
+                </form>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default SearchItems
